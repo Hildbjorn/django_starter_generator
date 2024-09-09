f204667e01b769b4ecd45826df07c2ca2e69ad8c
Hildbjorn <hildbjorn@gmail.com>
2024-09-09 12:07:02 +0300

    2024-09-09

---
+++ dsg_temp/src/core/views.py
@@ -0,0 +1,22 @@
+from django.views.generic import TemplateView
+
+
+# СДЕЛАТЬ: Удалить форму - это только тест
+from django import forms
+
+
+class MyForm(forms.Form):
+    name = forms.CharField(max_length=100)
+    email = forms.EmailField()
+
+
+class IndexPageView(TemplateView):
+    template_name = 'home/index.html'
+
+    # СДЕЛАТЬ: Удалить форму - это только тест
+    def get_context_data(self, **kwargs):
+        # Получаем контекст из родительского класса
+        context = super().get_context_data(**kwargs)
+        # Создаем экземпляр формы и добавляем его в контекст
+        context['form'] = MyForm()
+        return context
