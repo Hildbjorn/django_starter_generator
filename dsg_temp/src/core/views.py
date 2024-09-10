57e7271d2a2d187c3a31ba2edfefba276d790212
Hildbjorn <hildbjorn@gmail.com>
2024-09-10 11:40:04 +0300

    2024-09-10

--- dsg_temp/src/core/views.py
+++ dsg_temp/src/core/views.py
@@ -1,5 +1,18 @@
+from pathlib import Path
 from django.views.generic import TemplateView
 
 
 class IndexPageView(TemplateView):
     template_name = 'home/index.html'
+
+    def get_context_data(self, **kwargs):
+        # Получаем полный путь к директории проекта
+        project_path = Path(__file__).resolve().parent
+        # Извлекаем только название директории
+        project_name = str(project_path.name)
+
+        print('Проект: ', project_name)
+
+        context = super().get_context_data(**kwargs)
+        context['project_name'] = project_name
+        return context
